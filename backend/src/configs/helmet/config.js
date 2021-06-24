d5318a8a01c05c7eaad1ffe1c2ea16970cffaf4f
SFlorent <leyygaming@live.fr>
2021-06-24 13:11:44 +0200

    fix: change helmet options

--- backend/src/configs/helmet/config.js
+++ backend/src/configs/helmet/config.js
@@ -1,10 +1,5 @@
 module.exports = {
     HELMET_OPTIONS: {
-        contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-                "script-src": "'self' 'unsafe-inline'"
-            }
-        }
+        contentSecurityPolicy: false
     }
 }
