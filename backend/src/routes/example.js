574ba706996c878de128add86e27b463069f663b
Sfoucheur <s.foucheur@gmail.com>
2021-08-01 14:21:36 +0200

    Feat : added seperate files for swagger api models

--- backend/src/routes/example.js
+++ backend/src/routes/example.js
@@ -4,16 +4,6 @@ const router = express.Router();
 const models = require('../models');
 
 /**
- * @typedef User
- * @property {integer} id.required - Id - eg: 1
- * @property {string} login.required - Login - eg: Ivy234
- * @property {string} password.required - Password - eg: zbtLMEv8
- * @property {string} email.required - Email - eg: Ivy234@hotmail.com
- * @property {string} createdAt.required - Creation date - eg: 2021-04-27T08:34:01.000Z
- * @property {string} updatedAt.required - Update date - eg: 2021-04-27T08:34:01.000Z
- */
-
-/**
  * @route GET /example/users
  * @summary Return the users list
  * @group Example
---
+++ backend/src/routes/swaggerModels/exampleModels.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef User
+ * @property {integer} id.required - Id - eg: 1
+ * @property {string} login.required - Login - eg: Ivy234
+ * @property {string} password.required - Password - eg: zbtLMEv8
+ * @property {string} email.required - Email - eg: Ivy234@hotmail.com
+ * @property {string} createdAt.required - Creation date - eg: 2021-04-27T08:34:01.000Z
+ * @property {string} updatedAt.required - Update date - eg: 2021-04-27T08:34:01.000Z
+ */
